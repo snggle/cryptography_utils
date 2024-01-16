7fe11583faed30337510b72cef6fd13348a9b294
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2024-01-16 13:34:08 +0100

    Feature: AES DHKE v1
    This branch introduces the AESDHKE algorithm (version 1). AESDHKE is a cryptographic implementation combining AES (Advanced Encryption Standard) for symmetric data encryption and DHKE (Diffie-Hellman Key Exchange) for secure key establishment. It allows for secure data transmission over insecure channels by first establishing a shared secret key through DHKE and then using AES for data encryption and decryption.
    
    List of changes:
    - created aesdhke_v1.dart containing AESDHKE algorithm implementation
    - created ciphertext.dart storing a result of any encryption algorithm
    - created map_utils.dart containing general operations on maps
    - created encryption_algorithm_type.dart containing list of all available encryption algorithms (currently AESDHKE only)

--- lib/cryptography_utils.dart
+++ lib/cryptography_utils.dart
@@ -5,6 +5,7 @@ export 'src/bip/bip.dart';
 export 'src/cdsa/cdsa.dart';
 export 'src/config/config.dart';
 export 'src/encoding/encoding.dart';
+export 'src/encryption/encryption.dart';
 export 'src/hash/blake2b.dart';
 export 'src/hash/hmac.dart';
 export 'src/hash/keccak.dart';
