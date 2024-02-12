c9f3eca6abac6d2953eda0dc823002612cca4c9d
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2024-02-12 15:59:06 +0100

    Feature: EdDSA / ED25519 implementation
    EdDSA (Edwards-curve Digital Signature Algorithm) is a secure and efficient digital signature scheme using Edwards curves, with Ed25519 being its widely-used variant that offers fast operations through a 255-bit curve for enhanced cryptographic performance and security.
    
    List of changes:
    - created ed_point.dart containing operations related with Edwards curve calculations
    - created ed_curve.dart to store information about EDDSA curve
    - created ed25519_signer.dart to create and verify signatures for messages in accordance with ED25519 algorithm
    - created ed_signature.dart representing signature created by ed25519_signer.dart
    - created ed25519_private_key.dart and ed25519_public_key.dart which are the result of EdDSA-ED25519 algorithm operations
    - created ed25519_utils.dart containing helper methods for generic operations related to ED25519 curve
    - added Endian.little encoding to methods in big_int_utils.dart
    - added new generator point for ED25519 to curve_points.dart
    - added new curve definition for ED25519 to curves.dart

--- lib/src/cdsa/cdsa.dart
+++ lib/src/cdsa/cdsa.dart
@@ -2,3 +2,4 @@ export 'curve_points.dart';
 export 'curve_type.dart';
 export 'curves.dart';
 export 'ecdsa/ecdsa.dart';
+export 'eddsa/eddsa.dart';
--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -2,4 +2,5 @@ export 'big_int_utils.dart';
 export 'binary_utils.dart';
 export 'bytes_utils.dart';
 export 'ec_point_utils.dart';
+export 'ed25519_utils.dart';
 export 'secure_random.dart';
