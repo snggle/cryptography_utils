477386c64dbe950c1615ebdef2b5983e64b95f8a
Kamil Soboski <39964811+SucharMistrz@users.noreply.github.com>
2025-07-31 19:52:38 +0200

    Improvements after the initial CR

--- test/transactions/solana/address_lookup_table_test.dart
+++ test/transactions/solana/address_lookup_table_test.dart
@@ -6,22 +6,21 @@ import 'package:test/test.dart';
 void main() {
   group('Tests of AddressLookupTable', () {
     test('Should [construct AddressLookupTable] with correct fields', () {
-      // Arrange
+      // Act
+      AddressLookupTable actualAddressLookupTable = AddressLookupTable(
+        accountKey: Uint8List.fromList(List<int>.generate(32, (int i) => i)),
+        writableIndexesList: <int>[1, 2, 3],
+        readonlyIndexesList: <int>[4, 5, 6],
+      );
+
+      // Assert
       Uint8List expectedAccountKey = Uint8List.fromList(List<int>.generate(32, (int i) => i));
       List<int> expectedWritableIndexes = <int>[1, 2, 3];
       List<int> expectedReadonlyIndexes = <int>[4, 5, 6];
 
-      // Act
-      AddressLookupTable actualAddressLookupTable = AddressLookupTable(
-        accountKey: expectedAccountKey,
-        writableIndexes: expectedWritableIndexes,
-        readonlyIndexes: expectedReadonlyIndexes,
-      );
-
-      // Assert
       expect(actualAddressLookupTable.accountKey, expectedAccountKey);
-      expect(actualAddressLookupTable.writableIndexes, expectedWritableIndexes);
-      expect(actualAddressLookupTable.readonlyIndexes, expectedReadonlyIndexes);
+      expect(actualAddressLookupTable.writableIndexesList, expectedWritableIndexes);
+      expect(actualAddressLookupTable.readonlyIndexesList, expectedReadonlyIndexes);
     });
   });
 }
