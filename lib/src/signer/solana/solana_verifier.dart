2c9ca4bef93af6e44a3662f57c7bb9d9846afecf
Kamil Soboski <39964811+SucharMistrz@users.noreply.github.com>
2025-08-04 13:20:00 +0200

    Adjustments after rebase

--- lib/src/signer/solana/solana_verifier.dart
+++ lib/src/signer/solana/solana_verifier.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:cryptography_utils/cryptography_utils.dart';
-import 'package:cryptography_utils/src/cdsa/eddsa/ed25519/signer/ed25519_signer.dart';
+import 'package:cryptography_utils/src/cdsa/eddsa/ed25519/signer/ed25519_verifier.dart';
 import 'package:cryptography_utils/src/cdsa/eddsa/ed25519/signer/ed_signature.dart';
 
 class SolanaVerifier {
@@ -14,11 +14,11 @@ class SolanaVerifier {
     Uint8List s = solanaSignature.signatureBytes.sublist(32, 64);
     EDSignature edSignature = EDSignature(r: r, s: s);
 
-    ED25519Signer signer = ED25519Signer(
-      privateKey: _ed25519PrivateKey,
+    ED25519Verifier verifier = ED25519Verifier(
       hashFunction: Sha512(),
+      publicKey: _ed25519PrivateKey.publicKey,
     );
 
-    return signer.verifySignature(message, edSignature);
+    return verifier.isSignatureValid(message, edSignature);
   }
 }
